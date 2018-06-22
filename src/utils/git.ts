69f3938a0d3320376bd1fd7f482a78e0ea77dd73
Jeff Principe <princjef@gmail.com>
2018-06-21 22:00:12 -0700

    test: use latest npm

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -5,7 +5,7 @@ const BRANCH_COMMAND = 'git rev-parse --abbrev-ref HEAD';
 export async function currentBranch(): Promise<string> {
     try {
         return (await util.promisify(child_process.exec)(BRANCH_COMMAND)).stdout.trim();
-    } catch (e) {
+    } catch {
         throw new Error('Unable to fetch current git branch. Make sure you have git installed and are in a git repository.');
     }
 }
